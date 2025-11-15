12ad56731869e32127bdfa266ac845bd41781a18
jnsrup <139642316+jnsrup@users.noreply.github.com>
2025-11-15 22:48:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl'>habit tracker app</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
